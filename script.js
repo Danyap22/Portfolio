c628cce767998b6b69565dbd2b20dd789bd7afae
Danyap22 <>
2025-04-17 20:52:16 +0800

    adding pop up mesasge

--- script.js
+++ script.js
@@ -4,6 +4,21 @@ const prev = document.querySelector('.prev');
 const next = document.querySelector('.next');
 const wrap = document.querySelector('.img-wrap');
 const imgs = document.querySelectorAll('.img-wrap img');
+const openBtn = document.getElementById("openModal");
+const closeBtn = document.getElementById("closeModal");
+const modal = document.getElementById("modal");
+const closeModal = document.getElementById("closeModal");
+
+
+// Show the modal when the page is loaded
+window.onload = () => {
+  modal.classList.add("active");
+};
+
+// Close the modal when the user clicks the X button
+closeModal.onclick = () => {
+  modal.classList.remove("active");
+};
 
 //carousell function
 let idx = 0;
@@ -39,3 +54,4 @@ menuIcon.onclick = () => {
 menuIcon.onclick = () => {
     navLinks.classList.toggle('active');
 }
+
